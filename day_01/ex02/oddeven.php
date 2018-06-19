21b0b4a6f2ad59a45e91d7390c975778b8b002e4
DmitryZabrotsky <reezee512@gmail.com>
2018-06-19 16:53:28 +0300

    00 01 02

---
+++ day_01/ex02/oddeven.php
@@ -0,0 +1,24 @@
+#!/usr/bin/php
+<?php
+	echo "Enter a number: ";
+	$num = rtrim(fgets(STDIN));
+	
+	while (!feof(STDIN))
+	{
+		if (is_numeric($num))
+		{
+			if ($num % 2)
+				echo "The number $num is even\n";
+			else
+				echo "The number $num is odd\n";
+		}
+		else
+		{
+			echo "'$num' is not a number\n";
+		}
+		echo "Enter a number: ";
+		$num = rtrim(fgets(STDIN));
+	}
+
+	echo "\n";
+?>
