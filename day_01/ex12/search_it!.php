08976a5fedd897cfd1ce9106c896a35b3a1bc56f
DmitryZabrotsky <reezee512@gmail.com>
2018-06-20 15:15:45 +0300

    12

--- day_01/ex12/search_it!.php
+++ day_01/ex12/search_it!.php
@@ -3,5 +3,16 @@
 	if ($argc <= 2)
 		exit (1);
 	$key = $argv[1];
-	var_dump($argv);
+	foreach ($argv as $value) {
+		if (strstr($value, $key)) {
+			$i = strpos($value, $key);
+			$len = strlen($value);
+			$exploaded_arg = explode(":", $value);
+			if ($i == 0)
+				$res = ($exploaded_arg[1]);
+			else
+				$res = ($exploaded_arg[2]);
+		}
+	}
+	echo $res."\n";
 ?>
