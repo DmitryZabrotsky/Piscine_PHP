c22823087fdad25b9ba0639bd4d262c76aef5c7b
DmitryZabrotsky <reezee512@gmail.com>
2018-06-20 20:21:05 +0300

    01

--- day_02/ex01/one_more_time.php
+++ day_02/ex01/one_more_time.php
@@ -9,10 +9,47 @@
 	}
 
 	$day = strtolower($time[1]);
-	if ($day == "lundi" || $day == "mardi" || $day == "mercredi" || $day == "jeudi" || $day == "vendredi" || $day == "samedi" || $day == "dimanche");
-	else
+	if ($day == "lundi" || $day == "mardi" || $day == "mercredi" || $day == "jeudi" || $day == "vendredi" ||
+		$day == "samedi" || $day == "dimanche");
+	else {
 		echo "Wrong Format\n";
+		exit (1);
+	}
 
-	$month = strtolower($time[3])
-	if ($month == "" || $month == "" || $month == "" || $month == "" || $month == "" || $month == "" || $month == "" || $month == "" || $month == "" || $month == "" || $month == "" || $month == "" ||)
+	$month = strtolower($time[3]);
+	if ($month == "janvier")
+		$mm = 1;
+	elseif ($month == "fevrier")
+		$mm = 2;
+	elseif ($month == "mars")
+		$mm = 3;
+	elseif ($month == "avril")
+		$mm = 4;
+	elseif ($month == "mai")
+		$mm = 5;
+	elseif ($month == "juin")
+		$mm = 6;
+	elseif ($month == "juillet")
+		$mm = 7;
+	elseif ($month == "aout")
+		$mm = 8;
+	elseif ($month == "septembre")
+		$mm = 9;
+	elseif ($month == "octobre")
+		$mm = 10;
+	elseif ($month == "novembre")
+		$mm = 11;
+	elseif ($month == "decembre")
+		$mm = 12;
+	else {
+		echo "Wrong Format\n";
+		exit (1);
+	}
+
+	date_default_timezone_set("Europe/Paris");
+	$res = mktime($time[5], $time[6], $time[7], $mm, $time[2], $time[4]);
+	if (!$res)
+		echo "Wrong Format\n";
+	else
+		echo $res."\n";
 ?>
