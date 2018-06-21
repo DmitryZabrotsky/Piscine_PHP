ef3ed72646aa7e3179edab417a1da399f51d00b7
DmitryZabrotsky <reezee512@gmail.com>
2018-06-21 13:43:20 +0300

    02

--- day_02/ex02/magnifying_glass.php
+++ day_02/ex02/magnifying_glass.php
@@ -2,10 +2,17 @@
 <?php
 
 	if ($argc != 2)
-		exit (1);	
-
+		exit (1);
 	$file = file_get_contents($argv[1]);
-	var_dump($file);
-	preg_match_all("/(<a.*title=)(\".*\")(.*>.*<\/a>)/", $file, $a_tags);
-	var_dump($a_tags);
+	preg_match_all("/(<a.*>)(.*)(<\/a>)/", $file, $a_tags);
+	preg_match_all('/(<a).*(title="(.*)").*(<\/a>)/', $file, $titles);
+	foreach ($a_tags[2] as $value)
+		$file = str_replace($value, strtoupper($value), $file);
+	foreach ($titles[3] as $value)
+		$file = str_replace($value, strtoupper($value), $file);
+	if (preg_match_all("/(<a[^<]*>)([^<]*<)/", $file, $a_tags)){
+		foreach ($a_tags[2] as $value)
+			$file = str_replace($value, strtoupper($value), $file);
+	}
+	echo $file;
 ?>
