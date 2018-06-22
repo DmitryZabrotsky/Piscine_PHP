9b9f7518ea04585a316242b92fb93004aaf7ce5d
DmitryZabrotsky <reezee512@gmail.com>
2018-06-22 22:30:05 +0300

    04

---
+++ day_04/ex00/index.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+if ($_GET["submit"] == "OK" && $_GET["login"] != FALSE && $_GET["passwd"] != FALSE) {
+	$_SESSION["login"] = $_GET["login"];
+	$_SESSION["passwd"] = $_GET["passwd"];
+}
+
+?>
+
+<html>
+<body>
+	<form method="get" name="index.php">
+		Username: <input type="text" name="login" value="<?php echo $_SESSION["login"]; ?>">
+		<br/>
+		Password: <input type="text" name="passwd" value="<?php echo $_SESSION["passwd"]; ?>">
+		<input type="submit" name="submit" value="OK">
+	</form>
+</body>
+</html>
