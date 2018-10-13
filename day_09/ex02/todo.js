4a90dcd5b045e7042631e6681e24ac1d4d457a6d
DmitryZabrotsky <reezee512@gmail.com>
2018-10-13 23:22:11 +0300

    00 02

--- day_09/ex02/todo.js
+++ day_09/ex02/todo.js
@@ -1,43 +1,92 @@
-var uniqueID = (function() {var id = 0; return function() { return id++; };})();
+var c = document.cookie.split(';');
+console.log(c);
 
-readCookie();
+if (c[0]){
+	var len = c.length - 1;
+	for (var i = 0; i <= len; i++) {
+		arr = decodeURIComponent(c[i]).split('=');
+
+		var newdiv = document.createElement('div');
+		var textnode = document.createTextNode(arr[1]);
+		newdiv.classList.add("todo-div");
+		newdiv.appendChild(textnode);
+		newdiv.id = arr[0];
+		newdiv.onclick = function() {deltodo()};
+		ftlist = document.getElementById('ft_list');
+		ftlist.insertBefore(newdiv, ftlist.childNodes[0]);
+	}
+}
+
+function setCookie(name, value, options) {
+  options = options || {};
+
+  var expires = options.expires;
+
+  if (typeof expires == "number" && expires) {
+    var d = new Date();
+    d.setTime(d.getTime() + expires * 1000);
+    expires = options.expires = d;
+  }
+  if (expires && expires.toUTCString) {
+    options.expires = expires.toUTCString();
+  }
+
+  value = encodeURIComponent(value);
+
+  var updatedCookie = name + "=" + value;
+
+  for (var propName in options) {
+    updatedCookie += "; " + propName;
+    var propValue = options[propName];
+    if (propValue !== true) {
+      updatedCookie += "=" + propValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
 
 function addtodo() {
 	var todo = prompt('Declare new TO DO:', '500 push ups till 11:42 pm');
 	if (!todo) {
 		return ;
 	}
-	var id = uniqueID();
+	var id = Date.now();
 	var newdiv = document.createElement('div');
 	var textnode = document.createTextNode(todo);
 	newdiv.classList.add("todo-div");
 	newdiv.appendChild(textnode);
 	newdiv.id = id;
 	newdiv.onclick = function() {deltodo()};
-	ftlist = document.getElementById('ft_list')
+	ftlist = document.getElementById('ft_list');
 	ftlist.insertBefore(newdiv, ftlist.childNodes[0]);
-	updateCookie(newdiv, id);
+
+	setCookie(id, todo);
+}
+
+function deleteCookie(name) {
+  setCookie(name, "", {
+    expires: -1
+  })
 }
 
 function deltodo() {
 	var todo = event.target;
-	deleteCookie(todo);
 	todo.parentNode.removeChild(todo);
+	deleteCookie(todo.id);
 }
 
-function readCookie() {
-	var cookies = document.cookie;
-	var arr = cookies.split(';');
-	console.log(arr);
-}
 
-function updateCookie(element, id) {
-	var cookie = document.cookie;
-	document.cookie = id + "=" + element;
-}
 
-function deleteCookie(element) {
-	var id = element.id;
-	var date = new Date(0);
-	document.cookie = id + "=; expires=" + date.toUTCString();
-}
+
+
+
+
+
+
+
+
+
+
+
+
